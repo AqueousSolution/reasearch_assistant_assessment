20f555848c4dc53d26d5e843ddbac6e5051cd8cb
Idahosa <akioyaidahosa@yahoo.com>
2020-09-25 13:05:15 +0100

    Added proth.js

---
+++ proth.js
@@ -0,0 +1,57 @@
+    
+ const prothPrimeChecker = (num) =>{
+
+//This function checks if a number is a power of 2
+const isThisPowerOfTwo =(num)=>{
+	    	if (num&& !(num&(num-1))){
+	        return true;
+	    	}
+	    	else{
+	        return false;
+	    	}
+		}
+
+//This function checks if a number is a proth number cos we need to validate that the user is inputing a proth
+const isproth =(num) =>{
+
+	    let k=1;
+	        while (k<(num/k)){
+	            if((num%k)==0){
+	                if(isThisPowerOfTwo((num/k))){
+	                    return true;
+	                }
+	            }
+	            k=k+2;
+	        }
+	    return false;
+
+	}
+
+//This function checks if a proth number is a prime number
+	const prothPrimeChecker = (prothNumber) =>{
+	let arr = [];
+	let d = prothNumber - 1;
+	let power = d/2;
+	for(let a = 2; a<15; a++){
+		arr.push(Math.pow(a,power)+1)
+	}
+	const factorCheck = arr.map(el=>el/prothNumber);
+	
+   const isItPrime = factorCheck.some((e)=>Number.isInteger(e));
+	return isItPrime?'This number is prime':'This is not a prime number'
+}
+
+
+	let deductOne = num - 1;
+	if(isproth(deductOne)){
+		return prothPrimeChecker(num)
+}else{
+    return("This is not a proth number,please enter a proth number");
+}
+
+
+	}
+
+ 
+
+
